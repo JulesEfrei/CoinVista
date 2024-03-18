0ee6cf729de9080586686c5d668a355bab6fa34a
JulesEfrei <jules.bruzeau@efrei.net>
2024-03-18 13:59:18 +0100

    feat: navigation

--- src/components/atoms/ThemeSwitcher.tsx
+++ src/components/atoms/ThemeSwitcher.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "utils/Theme";
+import { FiSun } from "react-icons/fi";
+import { BsMoonStars } from "react-icons/bs";
 
 const ThemeSwitcher = () => {
-  const { switchTheme } = useContext(ThemeContext);
+  const { currTheme, switchTheme } = useContext(ThemeContext);
 
   return (
     <>
-      <button onClick={switchTheme}>Switch Theme</button>
+      <button
+        onClick={switchTheme}
+        className="border-1 border-gray-500 text-white py-2 px-3 rounded-md flex items-center h-full hover:border-white"
+      >
+        {currTheme === "dark" ? <BsMoonStars /> : <FiSun />}
+      </button>
     </>
   );
 };
