f7dec32a5bc65e2007b8e55416263d74953aaad1
JulesEfrei <jules.bruzeau@efrei.net>
2024-03-19 21:12:14 +0100

    feat: locales & translation

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navigation from "@molecules/Navigation";
-import ThemeProvider from "../utils/Theme";
+import ThemeProvider from "../../utils/Theme";
 
 export const metadata: Metadata = {
   title: "Home - Dashboard",
@@ -10,11 +10,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { lang: string };
 }>) {
   return (
-    <html lang="en">
+    <html lang={params.lang.split("-")[0] || "en"}>
       <ThemeProvider>
         <main>{children}</main>
         <Navigation />
---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,11 @@
+import { getTranslation } from "./translation";
+
+export default async function Home({ params }: { params: { lang: string } }) {
+  const translation = await getTranslation(params.lang.split("-")[0]);
+  return (
+    <>
+      <h1>{translation.home.title}</h1>
+      <p>{params.lang}</p>
+    </>
+  );
+}
